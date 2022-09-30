c445f4f04f6ce52a84f3173e12d289e8d74c7cde
Manuel <a20190133@pucp.edu.pe>
2022-09-30 08:53:43 -0500

    Avance adapter y clase mascota para el  recyclerView

---
+++ app/src/main/java/com/example/l3/ListaHistorialAdapter.java
@@ -0,0 +1,55 @@
+package com.example.l3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListaHistorialAdapter extends RecyclerView.Adapter<ListaHistorialAdapter.HistorialViewHolder> {
+
+    private Mascota[] listaMascotas;
+    private Context context;
+
+    public class HistorialViewHolder extends RecyclerView.ViewHolder {
+        Mascota mascota;
+
+        public HistorialViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public HistorialViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_historial, parent, false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistorialViewHolder holder, int position) {
+        Mascota mascota = listaMascotas[position];
+        holder.mascota = mascota;
+
+        TextView textViewInfo = holder.itemView.findViewById(R.id.item_mascota);
+        //Crear el string y adjuntarlo a la vista
+        String infoMascota =
+                        "Mascota: " + mascota.getMascota() + "\n" +
+                        "Género: " + mascota.getGenero() + "\n" +
+                        "Dueño: " + mascota.getDuenio() + "\n" +
+                        "DNI: " + mascota.getDni() + "\n" +
+                        "Descripción: " + mascota.getDescripcion() + "\n" +
+                        "Ruta: " + mascota.getRuta();
+        textViewInfo.setText(infoMascota);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaMascotas.length;
+    }
+
+}
---
+++ app/src/main/java/com/example/l3/Mascota.java
@@ -0,0 +1,59 @@
+package com.example.l3;
+
+public class Mascota {
+
+    private String mascota;
+    private String genero;
+    private String duenio;
+    private String dni;
+    private String descripcion;
+    private String ruta;
+
+    public String getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(String mascota) {
+        this.mascota = mascota;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+}
