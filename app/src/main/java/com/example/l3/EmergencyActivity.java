ee47c128d9b1d4123b135ce6e133d9d2d24688d3
PercyDLR <a20192265@pucp.edu.pe>
2022-09-30 09:43:16 -0500

    Avance vista emergencias

--- app/src/main/java/com/example/l3/EmergencyActivity.java
+++ app/src/main/java/com/example/l3/EmergencyActivity.java
@@ -2,7 +2,16 @@ package com.example.l3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class EmergencyActivity extends AppCompatActivity {
 
@@ -10,5 +19,24 @@ public class EmergencyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+    }
+
+    public void procesarDirección(View view){
+
+        EditText etDest = findViewById(R.id.etDestino);
+
+        Geocoder geocoder = new Geocoder(EmergencyActivity.this, Locale.US);
+        try {
+            Address direccion = geocoder.getFromLocationName(etDest.getText().toString(),1).get(0);
+
+            double longDest = direccion.getLongitude();
+            double latDest = direccion.getLatitude();
+
+            Log.d("direccion","Long: "+longDest+" | Lat: "+latDest);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
